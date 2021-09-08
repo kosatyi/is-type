881e496906552c6a824a0af9369ba4e0da7144c0
Stepan Kosatyi <stepan@kosatyi.com>
2021-09-08 09:16:40 +0300

    update

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,83 @@
-export function isOneOf(): boolean;
-export function getType(): boolean;
-export function isType(): boolean;
-export function isAnyObject(): boolean;
-export function isPlainObject(): boolean;
-export function isObject(): boolean;
-export function isEmptyObject(): boolean;
-export function isFullObject(): boolean;
-export function isObjectLike(): boolean;
-export function isFunction(): boolean;
-export function isRegexp(): boolean;
-export function isArguments(): boolean;
-export function isArray(): boolean;
-export function isEmptyArray(): boolean;
-export function isFullArray(): boolean;
-export function isNumber(): boolean;
-export function isElement(): boolean;
-export function isNull(): boolean;
-export function isString(): boolean;
-export function isEmptyString(): boolean;
-export function isFullString(): boolean;
-export function isBoolean(): boolean;
-export function isUndefined(): boolean;
-export function isMap(): boolean;
-export function isWeakMap(): boolean;
-export function isSet(): boolean;
-export function isWeakSet(): boolean;
-export function isSymbol(): boolean;
-export function isDate(): boolean;
-export function isBlob(): boolean;
-export function isFile(): boolean;
-export function isPromise(): boolean;
-export function isError(): boolean;
-export function isNaNValue(): boolean;
-export function isNullOrUndefined(): boolean;
-export function isPrimitive(): boolean;
+interface Schema extends Object {
 
-export function oneOf(): any;
-export function or(): any;
-export function and(): any;
-export function filter(): any;
+}
+
+export function isOneOf(...callbacks: Function[]): Function;
+
+export function getType(o: any): boolean;
+
+export function isType(o: any): boolean;
+
+export function isAnyObject(o: any): boolean;
+
+export function isPlainObject(o: any): boolean;
+
+export function isObject(o: any): boolean;
+
+export function isEmptyObject(o: any): boolean;
+
+export function isFullObject(o: any): boolean;
+
+export function isObjectLike(o: any): boolean;
+
+export function isFunction(o: any): boolean;
+
+export function isRegexp(o: any): boolean;
+
+export function isArguments(o: any): boolean;
+
+export function isArray(o: any): boolean;
+
+export function isEmptyArray(o: any): boolean;
+
+export function isFullArray(o: any): boolean;
+
+export function isNumber(o: any): boolean;
+
+export function isElement(o: any): boolean;
+
+export function isNull(o: any): boolean;
+
+export function isString(o: any): boolean;
+
+export function isEmptyString(o: any): boolean;
+
+export function isFullString(o: any): boolean;
+
+export function isBoolean(o: any): boolean;
+
+export function isUndefined(o: any): boolean;
+
+export function isMap(o: any): boolean;
+
+export function isWeakMap(o: any): boolean;
+
+export function isSet(o: any): boolean;
+
+export function isWeakSet(o: any): boolean;
+
+export function isSymbol(o: any): boolean;
+
+export function isDate(o: any): boolean;
+
+export function isBlob(o: any): boolean;
+
+export function isFile(o: any): boolean;
+
+export function isPromise(o: any): boolean;
+
+export function isError(o: any): boolean;
+
+export function isNaNValue(o: any): boolean;
+
+export function isNullOrUndefined(o: any): boolean;
+
+export function isPrimitive(o: any): boolean;
+
+export function oneOf(...args: any[]): any;
+
+export function or(...args: any[]): any;
+
+export function and(...args: any[]): any;
+
+export function filter(object: Object, schema: Schema): any;
